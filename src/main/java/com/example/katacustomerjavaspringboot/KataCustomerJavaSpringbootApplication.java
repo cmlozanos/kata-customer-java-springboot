3fd9893a2c0356241b027ac9c6ab96981515c9ac
Carlos Lozano <carlosmlozanos@gmail.com>
2020-11-09 17:27:17 +0100

    initial project

---
+++ src/main/java/com/example/katacustomerjavaspringboot/KataCustomerJavaSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.katacustomerjavaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataCustomerJavaSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KataCustomerJavaSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/katacustomerjavaspringboot/KataCustomerJavaSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.katacustomerjavaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KataCustomerJavaSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
