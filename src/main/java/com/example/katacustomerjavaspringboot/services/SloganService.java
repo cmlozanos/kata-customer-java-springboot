cdbaa499be471e5bf2f4ba665065648f2f0412d2
Carlos Lozano <carlosmlozanos@gmail.com>
2020-11-10 16:41:27 +0100

    fix test for slogan create at service test with maximun slogans per user exceeded

--- src/main/java/com/example/katacustomerjavaspringboot/services/SloganService.java
+++ src/main/java/com/example/katacustomerjavaspringboot/services/SloganService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.example.katacustomerjavaspringboot.domain.Slogan;
 import com.example.katacustomerjavaspringboot.domain.SloganRepository;
+import com.example.katacustomerjavaspringboot.exceptions.MaxSlogansPerUserException;
 
 @Service
 public class SloganService {
@@ -13,6 +14,10 @@ public class SloganService {
 	SloganRepository repository;
 
 	public Slogan create(final Slogan slogan) {
+		final Long countByUserId = this.repository.countByUserId(slogan.getUserId());
+		if (3 <= countByUserId) {
+			throw new MaxSlogansPerUserException();
+		}
 		return this.repository.save(slogan);
 	}
 
