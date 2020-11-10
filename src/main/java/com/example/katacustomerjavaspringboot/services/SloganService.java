52aec8412f2818dfcab18914f28867735146ed3b
Carlos Lozano <carlosmlozanos@gmail.com>
2020-11-10 16:04:16 +0100

    fix test for slogan create at service test

--- src/main/java/com/example/katacustomerjavaspringboot/services/SloganService.java
+++ src/main/java/com/example/katacustomerjavaspringboot/services/SloganService.java
@@ -1,15 +1,19 @@
 package com.example.katacustomerjavaspringboot.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.katacustomerjavaspringboot.domain.Slogan;
+import com.example.katacustomerjavaspringboot.domain.SloganRepository;
 
 @Service
 public class SloganService {
 
+	@Autowired
+	SloganRepository repository;
+
 	public Slogan create(final Slogan slogan) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.repository.save(slogan);
 	}
 
 }
