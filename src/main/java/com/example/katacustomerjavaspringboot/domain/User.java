f47c422b3c71a2804749b93abd70258d33b638e3
Carlos Lozano <carlosmlozanos@gmail.com>
2020-11-10 11:36:17 +0100

    add post mapping add no args constructor

--- src/main/java/com/example/katacustomerjavaspringboot/domain/User.java
+++ src/main/java/com/example/katacustomerjavaspringboot/domain/User.java
@@ -9,11 +9,15 @@ import javax.persistence.Id;
 
 import org.hibernate.annotations.GenericGenerator;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class User {
 	@Id
--- src/main/java/com/example/katacustomerjavaspringboot/web/controller/UserController.java
+++ src/main/java/com/example/katacustomerjavaspringboot/web/controller/UserController.java
@@ -4,6 +4,7 @@ import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,9 +18,10 @@ public class UserController {
 	@Autowired
 	public UserService service;
 
+	@PostMapping
 	public ResponseEntity<User> create(final User user) {
 		final User userCreated = this.service.create(user);
-		return ResponseEntity.created(URI.create("api/users/" + userCreated.getId())).body(userCreated);
+		return ResponseEntity.created(URI.create("api/users/" + userCreated.getId())).build();
 	}
 
 }
